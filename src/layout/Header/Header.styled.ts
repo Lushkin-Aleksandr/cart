7b4de3e8aa195546b837ed6af96eb88093b2eda8
sanyalu <sanyalu97@gmail.com>
2023-02-22 22:28:47 +0100

    feat(header): add logo & link that lead to cart page

--- src/layout/Header/Header.styled.ts
+++ src/layout/Header/Header.styled.ts
@@ -1,6 +1,30 @@
 import styled from 'styled-components'
 
 export const StyledHeader = styled.header`
-  height: 50px;
-  background: red;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px 20px;
+  background: #f8a64c;
+  box-shadow: 0 0 5px 0 #000000;
+
+  img {
+    width: 70px;
+    height: 70px;
+  }
+
+  a {
+    border: none;
+    border-radius: 25px;
+    padding: 10px 40px;
+    background-color: #323d39;
+    box-shadow: 0 0 2px 0 #000000;
+    color: #fff;
+    cursor: pointer;
+  }
+
+  a:hover {
+    background-color: #485953;
+    transform: scale(98%);
+  }
 `
--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,12 +1,16 @@
 import React, { FC } from 'react'
 import { StyledHeader } from './Header.styled'
+import logo from '../../common/assets/images/logo1.png'
+import { Link } from 'react-router-dom'
+import { RoutePaths } from '../../routes/AppRoutes'
 
 type PropsType = {}
 
 export const Header: FC<PropsType> = ({}) => {
   return (
     <StyledHeader>
-      <h1>Header</h1>
+      <img src={logo} alt={'logo'} />
+      <Link to={RoutePaths.CART}>Cart</Link>
     </StyledHeader>
   )
 }
