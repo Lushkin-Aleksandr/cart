1918ccbc5b354dd29907598d3b51bcfc3b78f7b8
sanyalu <sanyalu97@gmail.com>
2023-02-24 14:44:54 +0100

    feat(container): make container responsive

--- src/common/components/Container/Container.styled.ts
+++ src/common/components/Container/Container.styled.ts
@@ -4,5 +4,16 @@ export const StyledContainer = styled.div`
   max-width: 1160px;
   padding: 0 20px;
   margin: 0 auto;
-  border: 1px dotted #fff;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.lg}) {
+    max-width: 860px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.md}) {
+    max-width: 740px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.sm}) {
+    max-width: 540px;
+  }
 `
