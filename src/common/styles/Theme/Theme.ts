f752f7126b52806966e5715cceb6a5a105331401
sanyalu <sanyalu97@gmail.com>
2023-02-24 11:23:18 +0100

    feat(Theme): create theme

---
+++ src/common/styles/Theme/Theme.ts
@@ -0,0 +1,14 @@
+export type ThemeType = typeof theme
+
+export const theme = {
+  colors: {
+    primary: '#df9344',
+    secondary: '#323d39',
+    light: '#f5dece',
+  },
+  textColors: {
+    primary: '#342c27',
+    secondary: '#a08d84',
+    light: '#fff',
+  },
+}
