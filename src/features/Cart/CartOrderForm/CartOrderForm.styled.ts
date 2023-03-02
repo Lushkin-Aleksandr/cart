4b43a8a9f96f273c124c27a316c1a9c43c7dfe75
sanyalu <sanyalu97@gmail.com>
2023-03-02 19:05:41 +0100

    feat(cartorderform): create empty cardorderform

---
+++ src/features/Cart/CartOrderForm/CartOrderForm.styled.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const StyledCartOrderForm = styled.form`
+  border: 2px dashed #fff;
+  flex-basis: 400px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 500px;
+  font-size: 32px;
+  color: #fff;
+`
---
+++ src/features/Cart/CartOrderForm/CartOrderForm.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react'
+import { StyledCartOrderForm } from './CartOrderForm.styled'
+
+type PropsType = {}
+
+export const CartOrderForm: FC<PropsType> = () => {
+  return <StyledCartOrderForm>Form</StyledCartOrderForm>
+}
