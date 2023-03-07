6432e3573d75705cf36ad199880d4a040a769cc7
sanyalu <sanyalu97@gmail.com>
2023-03-07 21:44:19 +0100

    feat(Header): add Goods button

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,24 +1,33 @@
 import React, { FC } from 'react'
 import { StyledHeader } from './Header.styled'
 
-import { Link, useNavigate } from 'react-router-dom'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 import { RoutePaths } from '../../routes/AppRoutes'
 import { Logo } from '../../common/components/Logo/Logo'
 import { CartButton } from '../../common/components/CartButton/CartButton'
+import { Button } from '../../common/components/Button/Button'
 
 type PropsType = {}
 
 export const Header: FC<PropsType> = ({}) => {
   const navigate = useNavigate()
+  const location = useLocation()
 
   const handleCartButtonClick = () => navigate(RoutePaths.CART)
+  const handleBackToGoodsButtonClick = () => navigate(RoutePaths.GOODS)
 
   return (
     <StyledHeader>
       <Link to={RoutePaths.GOODS}>
         <Logo />
       </Link>
-      <CartButton onClick={handleCartButtonClick} commonPrice={200} />
+      {location.pathname === RoutePaths.CART ? (
+        <Button variant={'secondary'} onClick={handleBackToGoodsButtonClick}>
+          Goods
+        </Button>
+      ) : (
+        <CartButton onClick={handleCartButtonClick} commonPrice={200} />
+      )}
     </StyledHeader>
   )
 }
