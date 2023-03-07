6aa49a443bd57e7bcb5f99e792328b2afcdbe8ec
sanyalu <sanyalu97@gmail.com>
2023-03-07 20:57:07 +0100

    feat(CartOrderForm): make CartOrderForm layout

--- src/features/Cart/CartOrderForm/CartOrderForm.styled.ts
+++ src/features/Cart/CartOrderForm/CartOrderForm.styled.ts
@@ -1,14 +1,19 @@
 import styled from 'styled-components'
 
 export const StyledCartOrderForm = styled.form`
-  border: 2px dashed #fff;
+  background: ${({ theme }) => theme.colors.light};
+  border-radius: 10px;
   flex-basis: 400px;
   display: flex;
-  justify-content: center;
+  flex-direction: column;
   align-items: center;
-  height: 500px;
-  font-size: 32px;
+  padding: 20px;
   color: #fff;
+  gap: 15px;
+
+  h2 {
+    color: ${({ theme }) => theme.textColors.primary};
+  }
 
   @media (max-width: ${({ theme }) => theme.breakpoints.lg}) {
     flex-basis: 300px;
@@ -16,5 +21,23 @@ export const StyledCartOrderForm = styled.form`
 
   @media (max-width: ${({ theme }) => theme.breakpoints.md}) {
     flex-basis: auto;
+  }
+`
+
+export const CartOrderFormBottom = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+
+  span {
+    font-weight: 700;
+    color: ${({ theme }) => theme.textColors.primary};
+  }
+`
+export const CartOrderFormInputs = styled.div`
+  width: 100%;
+  & > div {
+    margin-bottom: 5px;
   }
 `
--- src/features/Cart/CartOrderForm/CartOrderForm.tsx
+++ src/features/Cart/CartOrderForm/CartOrderForm.tsx
@@ -1,8 +1,87 @@
 import React, { FC } from 'react'
-import { StyledCartOrderForm } from './CartOrderForm.styled'
+import {
+  CartOrderFormBottom,
+  CartOrderFormInputs,
+  StyledCartOrderForm,
+} from './CartOrderForm.styled'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
+import { Button } from '../../../common/components/Button/Button'
+import { Input } from '../../../common/components/Input/Input'
 
 type PropsType = {}
 
+const phoneRegExp = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im
+
+const orderSchema = Yup.object({
+  name: Yup.string()
+    .min(2, 'Min length is 2 characters!')
+    .max(50, 'Max length is 50 characters!')
+    .trim('Remove spaces!')
+    .strict(true)
+    .required('Name is required!'),
+  surname: Yup.string()
+    .min(2, 'Min length is 2 characters!')
+    .max(50, 'Max length is 50 characters!')
+    .trim('Remove spaces!')
+    .strict(true),
+  address: Yup.string()
+    .required('Address is required!')
+    .trim('Remove spaces!')
+    .strict(true)
+    .min(10, 'Min length is 10 characters!'),
+  phone: Yup.string()
+    .matches(phoneRegExp, 'Phone number is not valid')
+    .required('Phone is required!'),
+})
+
 export const CartOrderForm: FC<PropsType> = () => {
-  return <StyledCartOrderForm>Form</StyledCartOrderForm>
+  const { getFieldProps, errors, handleSubmit, touched } = useFormik({
+    initialValues: {
+      name: '',
+      surname: '',
+      address: '',
+      phone: '',
+    },
+    validationSchema: orderSchema,
+    onSubmit(values) {
+      console.log(values)
+    },
+  })
+
+  return (
+    <StyledCartOrderForm onSubmit={handleSubmit}>
+      <h2>Make order</h2>
+      <CartOrderFormInputs>
+        <Input
+          label={'Name'}
+          isRequired
+          {...getFieldProps('name')}
+          error={touched.name && errors.name}
+        />
+        <Input
+          label={'Surname'}
+          {...getFieldProps('surname')}
+          error={touched.surname && errors.surname}
+        />
+        <Input
+          label={'Address'}
+          isRequired
+          {...getFieldProps('address')}
+          error={touched.address && errors.address}
+        />
+        <Input
+          type={'tel'}
+          label={'Phone'}
+          isRequired
+          {...getFieldProps('phone')}
+          error={touched.phone && errors.phone}
+        />
+      </CartOrderFormInputs>
+      <CartOrderFormBottom>
+        <span>Total: 200$</span>
+        <Button type={'submit'}>Order</Button>
+      </CartOrderFormBottom>
+    </StyledCartOrderForm>
+  )
 }
