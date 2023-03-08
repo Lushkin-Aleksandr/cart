13d0bbeda522be64eb0c0e76f397e5e2e9a890ad
sanyalu <sanyalu97@gmail.com>
2023-03-08 23:14:16 +0100

    feat(floatMathOperations): create add and subtract functions

---
+++ src/common/utils/floatMathOperations.ts
@@ -0,0 +1,5 @@
+export const sumOfTwoFloatNumbers = (a: string, b: string) =>
+  (parseFloat(a) + parseFloat(b)).toFixed(2)
+
+export const subtractTwoFloatNumbers = (a: string, b: string) =>
+  (parseFloat(a) - parseFloat(b)).toFixed(2)
