5ddd89d2b8025ea94fb12d1a854e6ab986c121b8
sanyalu <sanyalu97@gmail.com>
2023-03-09 21:00:08 +0100

    feat(store): save state to localstorage

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,13 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { goodsReducer } from '../features/Goods/goodsSlice'
 import { cartReducer } from '../features/Cart/cartSlice'
+import { loadState, saveState } from '../common/utils/localStorage'
+import throttle from 'lodash.throttle'
 
 export const store = configureStore({
   reducer: {
     goods: goodsReducer,
     cart: cartReducer,
   },
+  preloadedState: loadState(), // load state from local storage
 })
 
+store.subscribe(throttle(() => saveState(store.getState()), 1000)) // Redo this after making backend (save only cart)
 export type RootStateType = ReturnType<typeof store.getState>
 export type AppDispatchType = typeof store.dispatch
