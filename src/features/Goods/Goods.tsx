4f6cff5ffd5d7ccef8ba79ab1826b56a210c7f88
sanyalu <sanyalu97@gmail.com>
2023-03-14 16:49:24 +0100

    feat(Goods): add Goods initializing

--- src/features/Goods/Goods.tsx
+++ src/features/Goods/Goods.tsx
@@ -1,16 +1,50 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect } from 'react'
 import { GoodsItemsWrapper, StyledGoods } from './Goods.styled'
 import { StyledContainer } from '../../common/components/Container/Container.styled'
 import { GoodsCard } from './GoodsCard/GoodsCard'
 import { Title } from '../../common/components/Title/Title.styled'
 import { useAppSelector } from '../../common/hooks/useAppSelector'
-import { selectGoodsItems } from './goodsSelectors'
+import { selectGoodsItems, selectGoodsStatus } from './goodsSelectors'
 import { GoodType } from '../../api/api'
+import { useAppDispatch } from '../../common/hooks/useAppDispatch'
+import { getGoods, RequestStatusType } from './goodsSlice'
+import { Loader } from '../../common/components/Loader/Loader.styled'
 
 type PropsType = {}
 
 export const Goods: FC<PropsType> = ({}) => {
+  const dispatch = useAppDispatch()
   const goodsItems = useAppSelector<GoodType[]>(selectGoodsItems)
+  const isInitialized = useAppSelector<RequestStatusType>(selectGoodsStatus)
+
+  useEffect(() => {
+    dispatch(getGoods())
+  }, [])
+
+  if (isInitialized === 'loading') {
+    return (
+      <div
+        style={{
+          position: 'fixed',
+          width: '100%',
+          height: '100%',
+          zIndex: '999',
+        }}
+      >
+        <h1
+          style={{
+            position: 'fixed',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+          }}
+        >
+          Loading...
+        </h1>
+        <Loader />
+      </div>
+    )
+  }
 
   return (
     <StyledGoods>
--- src/features/Goods/goodsSelectors.ts
+++ src/features/Goods/goodsSelectors.ts
@@ -1,3 +1,4 @@
 import { RootStateType } from '../../app/store'
 
 export const selectGoodsItems = (state: RootStateType) => state.goods.items
+export const selectGoodsStatus = (state: RootStateType) => state.goods.status
